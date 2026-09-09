a28dee8342e55e612a95bb423c65e43c837ad69e
Caique <devcaiqueoliveira@gmail.com>
2026-09-09 11:38:07 -0300

    test: add UserServiceTest and createUserTest foundation

---
+++ src/test/java/com/devcaiqueoliveira/nexus_api/service/UserServiceTest.java
@@ -0,0 +1,50 @@
+package com.devcaiqueoliveira.nexus_api.service;
+
+import com.devcaiqueoliveira.nexus_api.dto.UserRequest;
+import com.devcaiqueoliveira.nexus_api.dto.UserResponse;
+import com.devcaiqueoliveira.nexus_api.entity.User;
+import com.devcaiqueoliveira.nexus_api.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.util.ReflectionTestUtils;
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+
+import java.util.UUID;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    @DisplayName("Deve criar um usúario com sucesso")
+    void createUserTest() {
+        UserRequest request = new UserRequest("Teste", "teste@teste.com", "123456")
+
+        when(userRepository.existsByEmail(request.email())).thenReturn(false);
+
+        when(passwordEncoder.encode(request.password())).thenReturn("senhacriptografada");
+
+        when(userRepository.save(any(User.class))).thenAnswer(invocation -> {
+            User savedUser = invocation.getArgument(0);
+            ReflectionTestUtils.setField(savedUser, "id", UUID.randomUUID());
+            return savedUser;
+        });
+
+        UserResponse response = userService.createUser(request);
+    }
+}
